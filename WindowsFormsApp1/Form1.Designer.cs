1119369b45f0ed29a95898295f256bc18c8f4726
Thomas Harrison <RC0024018@student.riversidecollege.ac.uk>
2017-09-27 10:04:00 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Question = new System.Windows.Forms.Button();
+            this.Yes = new System.Windows.Forms.Button();
+            this.No = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(422, 216);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Add name here";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Question
+            // 
+            this.Question.Location = new System.Drawing.Point(318, 203);
+            this.Question.Name = "Question";
+            this.Question.Size = new System.Drawing.Size(75, 23);
+            this.Question.TabIndex = 2;
+            this.Question.Text = "Question";
+            this.Question.UseVisualStyleBackColor = true;
+            this.Question.Click += new System.EventHandler(this.Question_Click);
+            // 
+            // Yes
+            // 
+            this.Yes.Location = new System.Drawing.Point(329, 287);
+            this.Yes.Name = "Yes";
+            this.Yes.Size = new System.Drawing.Size(75, 23);
+            this.Yes.TabIndex = 3;
+            this.Yes.Text = "Yes";
+            this.Yes.UseVisualStyleBackColor = true;
+            this.Yes.Click += new System.EventHandler(this.Yes_Click);
+            // 
+            // No
+            // 
+            this.No.Location = new System.Drawing.Point(447, 299);
+            this.No.Name = "No";
+            this.No.Size = new System.Drawing.Size(75, 23);
+            this.No.TabIndex = 4;
+            this.No.Text = "No";
+            this.No.UseVisualStyleBackColor = true;
+            this.No.Click += new System.EventHandler(this.No_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 417);
+            this.Controls.Add(this.No);
+            this.Controls.Add(this.Yes);
+            this.Controls.Add(this.Question);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button Question;
+        private System.Windows.Forms.Button Yes;
+        private System.Windows.Forms.Button No;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Question_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome " + textBox1.Text + " Question: 17 - 2 x 8 = 1, yes or no?");
+        }
+
+        private void Yes_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(" Correct rkid");
+        }
+
+        private void No_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(" Incorrect");
+        }
+    }
+}
